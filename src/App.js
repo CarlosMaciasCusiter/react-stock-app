73d0f0867c962ce6a26241bdca61cc9a3579dd0f
Carlos Macias Cusiter <CarlosMaciasCusiter@gmail.com>
2021-01-27 14:09:27 -0800

    Initial Git Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, {useState} from 'react';
+import Nav from './components/Nav/Nav.jsx';
 import './App.css';
 
+function Home() {
+  return <h1>Home Page</h1>
+}
+
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Nav isLoggedIn={isLoggedIn}/>
+    // <Router>
+    //   <div>
+    //     {/* <nav>
+    //       <ul>
+    //         <li>
+    //           <Link to='/home'>Home</Link>
+    //         </li>
+    //       </ul>
+    //     </nav> */}
+
+    //     <Switch>
+    //       <Route path='/home'>
+    //         <Home />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 
